eb58d331524f5145338e566bf86256359c9dca11
clementlemon02 <clementling02@gmail.com>
2025-05-13 17:23:18 +0800

    google auth

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://api.macdsignal.com/"],  # Frontend URL
+    allow_origins=["http://localhost:5173","https://macdsignal.com/"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
